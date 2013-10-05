e26d6e5d1b35ca10a00e0a3ae66eb30e646f0a9c
dan rassi <dan@rassi.net>
2013-10-05 02:06:07 +0000

    initial commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+beerstock README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/initialize_beerstock_db development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ beerstock/views.py
@@ -0,0 +1,10 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+
+from sqlalchemy.exc import DBAPIError
+
+from .models import DBSession, MyModel
+
+@view_config(route_name='home', renderer='home.mako')
+def my_view(request):
+    return {}
